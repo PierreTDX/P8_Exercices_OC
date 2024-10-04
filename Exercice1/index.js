d8d56e4a716f421813354b471dfdab9f68fa1b7f
PierreTDX <pierre.tondeux@gmail.com>
2024-10-04 10:09:31 +0200

    Init projet

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,22 @@
 //votre code ici
+function pairNumbers(min, max) {
+    let result = '';
 
-export default pairNumbers
+    for (let i = min; i <= max; i++) {
+        if (i % 2 === 0) {
+            result += i + ',';
+        }
+    }
+
+    // Retirer la dernière virgule
+    if (result.length > 0) {
+        result = result.slice(0, -1);
+    }
+
+    return result;
+}
+
+// Exemple d'utilisation
+console.log("Exercice1 : ",pairNumbers(1, 10));  // Retourne "2,4,6,8,10"
+
+// export default pairNumbers
